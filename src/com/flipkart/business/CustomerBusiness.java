68014816981740cc9d805899903a89f157448328
Aman-akt <108472341+Aman-akt@users.noreply.github.com>
2024-07-16 17:05:41 +0530

    First comment

---
+++ src/com/flipkart/business/CustomerBusiness.java
@@ -0,0 +1,21 @@
+package com.flipkart.business;
+
+public class CustomerBusiness {
+    public void createCustomer() {
+        System.out.println("Creating new customer..." + customerID);
+
+    }
+
+        public boolean deleteCustomer(int customerID) {
+            System.out.println("Deleting customer..." + customerID);
+            return true;
+        }
+        public boolean updateCustomer(int customerID) {
+            System.out.println("Updating customer..." + customerID);
+            return true;
+        }
+
+        public void listCustomers() {
+            System.out.println("Listing all customers...");
+        }
+}
